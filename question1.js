1ee7b80ee6e0be887695ff40d9daca83694ba538
serdarisikli <33202934+serdarisikli@users.noreply.github.com>
2022-04-10 17:34:37 -0500

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,24 @@
+const student = {
+    firstName: '', lastName: '', grades: [], inputNewGrade: function (newGrade) {
+        this.grades.push(newGrade);
+    }, computeAverageGrade() {
+        return this.grades.reduce((sum, current, index, array) => sum + current / array.length, 0);
+    }
+}
+const stu1 = Object.create(student);
+stu1.firstName = 'Serdar';
+stu1.lastName = 'Isikli';
+stu1.inputNewGrade(88);
+stu1.inputNewGrade(98);
+stu1.inputNewGrade(86);
+stu1.inputNewGrade(80);
+const stu2 = Object.create(student);
+stu2.firstName = 'Arin';
+stu2.lastName = 'Isikli';
+stu2.inputNewGrade(85);
+stu2.inputNewGrade(95);
+stu2.inputNewGrade(85);
+stu2.inputNewGrade(70);
+const students = [stu1, stu2];
+const result = students.reduce((average, stu, index, array) => average + stu.computeAverageGrade() / array.length, 0);
+console.log(result);
---
+++ question3.js
@@ -0,0 +1,15 @@
+Array.prototype.mysort = function () {
+    let arr = this;
+    let len = arr.length;
+    for (let i = len - 1; i >= 0; i--) {
+        for (let j = 1; j <= i; j++) {
+            if (arr[j - 1] > arr[j]) {
+                let temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log([4, 8, 7, 2, 5, 1].mysort());
